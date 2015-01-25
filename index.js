17085299fdbb33fc39c2b1470844439c9c1aabff
Saurav Kumar <2020saurav@gmail.com>
2015-01-25 17:02:40 +0530

    create server using express

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello")
+var http = require('http');
+var app = express();
+app.set('port', process.env.PORT||3000);
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
