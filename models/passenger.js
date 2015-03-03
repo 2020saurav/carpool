5c7eeedb4ac8d526dd990fb630932586c93999c2
Pawan Patel <patelpawan456@gmail.com>
2015-03-03 21:49:12 +0530

    created passenger model

--- models/passenger.js
+++ models/passenger.js
@@ -1,3 +1,25 @@
 /**
  * Created by saurav on 26/2/15.
  */
+/*
+ * @param sequelize
+ * @param DataTypes
+ * @returns {*|Model}
+ */
+module.exports = function(sequelize, DataTypes) {
+    var Passenger =  sequelize.define ('passenger',
+        {
+            id: {type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true},
+            journey_id: {type: DataTypes.INTEGER, allowNull: false},
+            user_id: {type: DataTypes.INTEGER, allowNull: false},
+            status: {type: DataTypes.INTEGER, allowNull: false, default:0}
+        },
+        {
+            freezeTableName : true,
+            underscored : true,
+            tableName : 'passenger',
+            timestamps : false
+        });
+
+    return Passenger;
+};
