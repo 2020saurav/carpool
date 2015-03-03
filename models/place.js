ed1f25bdf427ae7f5916945d5b6f5095e5ed5782
Saurav Kumar <2020saurav@gmail.com>
2015-03-03 21:41:05 +0530

    place model added

--- models/place.js
+++ models/place.js
@@ -1,3 +1,19 @@
-/**
- * Created by saurav on 26/2/15.
+/*
+ * @param sequelize
+ * @param DataTypes
+ * @returns {*|Model}
  */
+module.exports = function(sequelize, DataTypes) {
+    var Place =  sequelize.define ('place',
+        {
+            id: {type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true},
+            name: {type: DataTypes.STRING, allowNull: false}
+        },
+        {
+            freezeTableName : true,
+            underscored : true,
+            tableName : 'place',
+            timestamps : false
+        });
+    return Place;
+};
