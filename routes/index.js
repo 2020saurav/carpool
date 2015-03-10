9fdb4e39520764284c5416444843560164cddaaa
Pawan Patel <patelpawan456@gmail.com>
2015-03-10 20:40:58 +0530

    crated postregistration to handle the registratioon form

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 exports.login = require('./login');
 exports.postLogin = require("./postLogin");
-
+exports.registration = require("./registration");
+exports.postRegistration = require("./postRegistration");
 exports.testDBpost = require('./testDBpost');
 exports.testTravel = require('./testTravel');
 exports.testTravelPost = require('./testTravelPost');
